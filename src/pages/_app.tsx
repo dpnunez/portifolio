d1693da948fe59ef0aa5a4efb80cf8e2a3219c0a
Daniel Porto Nunez <daniel.portonunez@gmail.com>
2022-04-07 22:17:57 -0300

    feat: setup chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
 export default MyApp;
